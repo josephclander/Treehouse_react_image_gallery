a53280b5704472b00e425ea4120d9afe6a2b9753
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-27 12:18:23 +0000

    Edit: list items added to App state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import Header from './components/Header';
 import Gallery from './components/Gallery';
 
 class App extends Component {
+  state = {
+    images: [
+      {
+        src: 'https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg',
+        id: 1,
+      },
+      {
+        src: 'https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg',
+        id: 2,
+      },
+      {
+        src: 'https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg',
+        id: 3,
+      },
+      {
+        src: 'https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg',
+        id: 4,
+      },
+    ],
+  };
+
   render() {
     return (
       <div className='container'>
         <Header />
-        <Gallery />
+        <Gallery images={this.state.images} />
       </div>
     );
   }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-const images = [
-  {
-    src: 'https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg',
-    id: 1,
-  },
-  {
-    src: 'https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg',
-    id: 2,
-  },
-  {
-    src: 'https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg',
-    id: 3,
-  },
-  {
-    src: 'https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg',
-    id: 4,
-  },
-];
-
-const Gallery = () => {
-  const imageList = images.map((item) => {
+const Gallery = (props) => {
+  const imageList = props.images.map((item) => {
     return <ListItem source={item.src} key={item.id} />;
   });
   return (
