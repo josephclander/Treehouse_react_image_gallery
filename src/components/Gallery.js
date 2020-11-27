cf41de9da8605be73e634a8983a057d20bb9b3da
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-11-27 12:23:40 +0000

    Add NotFoundItem and functionality

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import ListItem from './ListItem';
+import NotFoundItem from './NotFoundItem';
 
 const Gallery = (props) => {
-  const imageList = props.images.map((item) => {
-    return <ListItem source={item.src} key={item.id} />;
-  });
+  let imageList;
+  if (props.images.length > 0) {
+    imageList = props.images.map((item) => {
+      return <ListItem source={item.src} key={item.id} />;
+    });
+  } else {
+    imageList = <NotFoundItem />;
+  }
   return (
     <div className='photo-container'>
       <h2>Results</h2>
-      <ul>
-        {imageList}
-
-        {/* Not Found */}
-        {/* <li class='not-found'>
-          <h3>No Results Found</h3>
-          <p>You search did not return any results. Please try again.</p>
-        </li> */}
-      </ul>
+      <ul>{imageList}</ul>
     </div>
   );
 };
