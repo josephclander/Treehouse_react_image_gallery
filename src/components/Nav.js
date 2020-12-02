5e6726d12f4a2a2147b2cff97584648c18a8ab30
Joseph Lander <josephlander@pjs-macbook-pro.lan>
2020-12-02 14:21:00 +0000

    Add NavLink to Nav, adjust styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav className='main-nav'>
       <ul>
-        <li>Cats</li>
-        <li>Dogs</li>
-        <li>Computers</li>
+        <li>
+          <NavLink to={'/search/cats'}>Cats</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/search/dogs'}>Dogs</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/search/computers'}>Computers</NavLink>
+        </li>
       </ul>
     </nav>
   );
